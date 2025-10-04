0844a882beab9039f0fbf896976d3c47056f4da0
Gustavo Queiroz <gustavoq.mateusgithub@gmail.com>
2025-10-04 19:21:13 -0300

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { AppHeader } from '@/components/AppHeader'
+import { Home } from '@/pages/Home'
+import { Quiz } from '@/pages/Quiz'
+import { Resultado } from '@/pages/Resultado'
+import { Ranking } from '@/pages/Ranking'
+import { Config} from '@/pages/Config'
+import { ThemeProvider } from '@/state/theme'
+import { QuizProvider } from '@/state/quiz'
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <QuizProvider>
+        <AppHeader />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/resultado" element={<Resultado />} />
+          <Route path="/ranking" element={<Ranking />} />
+          <Route path="/config" element={<Config />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </QuizProvider>
+    </ThemeProvider>
+  )
+}
