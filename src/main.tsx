ed1169b0d54e5c44e7189b7dfd57dd25c4cba1e7
Gustavo Queiroz <gustavoq.mateusgithub@gmail.com>
2025-10-07 10:01:29 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/theme.css'
+/**
+ * Ponto de entrada da aplicação.
+ * - Monta React StrictMode
+ * - Envolve a aplicação em BrowserRouter
+ * - Registra Service Worker (quando suportado) para PWA / offline básico
+ */
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
